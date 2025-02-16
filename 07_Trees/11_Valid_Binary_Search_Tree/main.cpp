35bae21d3a58b487d44b59c0d9c6ab5766c31189
Juan Ramón del Caño Vega <delcanovega@gmail.com>
2025-02-16 18:05:47 +0100

    Solved "Valid Binary Search Tree"

---
+++ 07_Trees/11_Valid_Binary_Search_Tree/main.cpp
@@ -0,0 +1,114 @@
+#include <cassert>
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return dfs(root, -1001, 1001);      
+    }
+private:
+    bool dfs(TreeNode* node, int left_bound, int right_bound) {
+        if (!node) {
+            return true;
+        }
+        if (node->val <= left_bound || node->val >= right_bound) {
+            return false;
+        }
+        
+        return dfs(node->left, left_bound, node->val)
+            && dfs(node->right, node->val, right_bound);
+    }
+};
+
+TreeNode* create_binary_tree(const vector<int>& values) {
+    if (values.empty()) {
+        return nullptr;
+    }
+
+    TreeNode* root = new TreeNode(values[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+
+    size_t i = 1;
+    while (i < values.size()) {
+        TreeNode* curr = q.front();
+        q.pop();
+
+        if (i < values.size() && values[i] != -1) {
+            curr->left = new TreeNode(values[i]);
+            q.push(curr->left);
+        }
+        i++;
+
+        if (i < values.size() && values[i] != -1) {
+            curr->right = new TreeNode(values[i]);
+            q.push(curr->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void delete_tree(TreeNode* root) {
+    if (!root) {
+        return;
+    }
+    delete_tree(root->left);
+    delete_tree(root->right);
+    delete root;
+}
+
+vector<int> level_order_traversal(TreeNode* root) {
+    vector<int> result;
+    if (!root) {
+        return result;
+    }
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+        result.push_back(node->val);
+
+        if (node->left) {
+            q.push(node->left);
+        }
+        if (node->right) {
+            q.push(node->right);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    Solution sol;
+
+    vector<int> tree{2,1,3};
+    TreeNode* root = create_binary_tree(tree);
+    assert(sol.isValidBST(root));
+
+    delete_tree(root);
+
+    tree = {5,4,6,-1,-1,3,7};
+    root = create_binary_tree(tree);
+    assert(!sol.isValidBST(root));
+
+    delete_tree(root);
+
+    return 0;
+}
